50351189523e50399a5c9eceea1efbc9642fd948
rawandrrr <>
2021-06-19 13:45:01 +0000

    new file

---
+++ App.js
@@ -0,0 +1,50 @@
+
+var firstName = "rawand"
+console.log(firstName)
+console.log("HELLO ALL")
+
+
+if(10 === 4){
+  console.log("yes it is")
+}
+else{
+  console.log("no it is not")
+}
+
+
+if(10 < 8){
+console.log("yes")
+}
+else if (20 > 15)
+{
+console.log("20 greater than 15")
+}
+else{
+  console.log("no no")
+}
+
+
+
+alert("welcome to pest sport web") +name
+var name = prompt("what is ur name" + name)
+
+
+var favsport = prompt("what is ur fav sport from these (football, tennis, swimming?")
+
+if(favsport == "football"){
+  document.write("<p>"+ "great thing that u love football cos we have it here in our webpage" +"</p>")
+
+}
+else if (favsport == "tennis")
+{
+document.write("<p>"+ "ohhh so u can see it here in our web" +"</p>")
+
+}
+else if (favsport == swimming)
+{
+document.write("<p>"+ "that good coz we have about it here" +"</p>")
+}
+
+else{
+  alert(" sorry ")
+}
