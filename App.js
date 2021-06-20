699563eda6644c65ff14d2512a72c27732514f93
rawandrrr <>
2021-06-20 20:44:54 +0000

    jj

--- App.js
+++ App.js
@@ -1,103 +1,66 @@
+var name = prompt("what is ur name" )
 
-var firstName = "rawand"
-console.log(firstName)
-console.log("HELLO ALL")
-
-
-if(10 === 4){
-  console.log("yes it is")
-}
-else{
-  console.log("no it is not")
-}
-
-
-if(10 < 8){
-console.log("yes")
-}
-else if (20 > 15)
-{
-console.log("20 greater than 15")
-}
-else{
-  console.log("no no")
-}
-
-
-
-alert("welcome to pest sport web") +name
-var name = prompt("what is ur name" + name)
+alert("welcome to pest sport web" + name)
 
 
 var favsport = prompt("what is ur fav sport from these (football, tennis, swimming)?")
 
 
 
-
-
-
-
-
-if(favsport == "football"){
-  document.write("<p>"+ "great thing that u love football cos we have it here in our webpage" +"</p>")
+while(favsport != "football" && favsport != "tennis" && favsport != "swimming" ){
+  favsport = prompt("what is ur fav sport from these (football, tennis, swimming)?")
 
 }
-else if (favsport == "tennis")
-{
-document.write("<p>"+ "ohhh so u can see it here in our web" +"</p>")
+
+
+
+
+if (favsport == "football") {
+  document.write("<p>" + "great thing that u love football cos we have it here in our webpage" + "</p>")
 
 }
-else if (favsport == swimming)
-{
-document.write("<p>"+ "that good coz we have about it here" +"</p>")
+else if (favsport == "tennis") {
+  document.write("<p>" + "ohhh so u can see it here in our web" + "</p>")
+
+}
+else if (favsport == "swimming") {
+  document.write("<p>" + "that good coz we have about it here" + "</p>")
 }
 
-else{
+else {
   alert(" sorry ")
 
 }
 
+rating ()
+
+function rating() {
+
+  var sport = prompt(" what is ur favsport of these? (football , tennis , swimming)?")
 
 
+  if (sport == "football") {
+    document.write("<p>" + "GREAT" + "</P>")
+  }
 
-function rating (){
-
-var sport -  prompt( " what is ur favsport of these? (football , tennis , swimming)?")
-
-
-if (sport == "football"){document.write("<p>" + "GREAT" + "</P>")
-}
-
-else if (favsport == tennis){document.write("<p>" + "ohhh u can see about it here" + "</P>")}
+  else if (favsport == tennis) { document.write("<p>" + "ohhh u can see about it here" + "</P>") }
 
 
-else {
-  alert ("sorry")
-}
-
-========================================
-                 function 
-
-
-
-function rating (){
-  var starnum - prompt("how many stars do u rate football?")
-
-  return starnum ;
-}
-
-var num = rating();
-for (var i=0 , i<num ; i++){document.write(
-  "<http://res.publicdomainfiles.com/pdf_view/54/13527173411344.png>")
+  else {
+    alert("sorry")
   }
 
 
+}
 
-function rating2 (num){
+function rating2() {
+  var starnum = Number (prompt("how many stars do u rate football?"))
 
-  for (var i=0 ; i<num ; i++){"<http://res.publicdomainfiles.com/pdf_view/54/13527173411344.png>")
-
+  for (var i = 0; i < starnum; i++) {
+    document.write(
+      "<img src ='http://res.publicdomainfiles.com/pdf_view/54/13527173411344.png'>")
 
   }
-
 }
+rating2();
+
